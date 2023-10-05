9d8aab7b04044f21ad7bfe377dd8bb3482ac2677
Vinícius Tadeu Andrade Costa <45463783+ViniciusTAC@users.noreply.github.com>
2023-10-05 10:42:10 -0300

    Alterações

--- projetoatividades/src/App.vue
+++ projetoatividades/src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-app-bar app color="#3E3E96" dark>
+      <div class="d-flex align-center">
+        <v-img alt="Logo" class="shrink mr-2" contain src="./assets/lista-de-afazeres.png" transition="scale-transition"
+          width="40" />
+
+        <span class="mr-2">Projeto Quadro de Atividades</span>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn href="./Teste.vue" target="_blank" text>
+        <span class="mr-2">Adicionar Atividade</span>
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld, 
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ projetoatividades/src/Teste.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container" name="franquias">
+      <h3>Faça parte você também da <span>nossa rede de franquias</span></span></h3>
+      <div class="row">
+          <div v-for="franquia in franquias" :key="franquia.id_franquia" class="col-md-2">
+              <router-link  :to="{ name: 'interna-franquia', params: { id: franquia.id_franquia }}">
+              <div class="blocks"><img :src="franquia.imagem" alt=""></div>
+              <h4>{{ franquia.titulo }}</h4>
+              </router-link>
+          </div>
+      </div>
+      <router-link to="franqueado"><div class="bt">seja um franqueado</div></router-link>
+      <!--<a href="/franqueado"><div class="bt">seja um franqueado</div></a>-->
+    </div>
+  </template>
+  
+  <script>
+  export default {
+      data() {
+          return {
+              franquias: []
+          }
+      },
+      created() {
+        this.$http.get('franquia_site')
+            .then(res => res.json())
+            .then(franquias => this.franquias = franquias, err => console.log(err));
+      }
+  }
+  </script>
